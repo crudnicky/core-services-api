05249b21fc689503597387db74461a99bc7c40bd
Chris Rudnicky <crudnicky@abscibio.com>
2021-10-11 16:51:20 -0700

    add version control and route to all plasmids

--- app/main.py
+++ app/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
-import requests
-
+import requests, okta_jwt_verifier, os
 from .routers import loupe
+from dotenv import load_dotenv
+from starlette.middleware.cors import CORSMiddleware
+
+load_dotenv()
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.on_event("startup")
 async def startup_event():
